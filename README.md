e6b9625d7af66b735adde51318d7f58b5a68cc05
Keefe Roedersheimer <keefe@categorize.us>
2022-02-19 20:18:24 -0500

    connect to rudimentary slack API

--- README.md
+++ README.md
@@ -4,3 +4,29 @@ conversational API client for Slack
 # Getting Started
 
 1. Visit [Slack](https://api.slack.com/start) sign up for an account, take a look at the API and ping me your email
+2. Create a new slack app and generate various tokens, e.g. 
+App ID 
+Date of App Creation February 19, 2022
+Client ID 
+Client Secret 
+Signing Secret 
+Verification Token 
+OAuth Tokens for Your Workspace 
+Save these carefully somewhere.
+3. Install jdk 11, eclipse or intellij
+4. Edit your environment
+kroeders@kroederss-MacBook-Pro interlocutor % cat ~/.zshenv 
+export SLACK_BOT_TOKEN=
+export SLACK_SIGNING_SECRET=
+export PATH="/usr/local/opt/openjdk@11/bin:$PATH"
+export JAVA_HOME=$(/usr/libexec/java_home)
+5. run ngrok to get a temporary URI forwarded to 3000, e.g. 
+                                          
+Web Interface                 http://127.0.0.1:4040                                                                                                                    
+Forwarding                    http://70e3-2600-1009-b1e6-a22f-a0b4-7cfc-6d81.ngrok.io -> http://localhost:3000                                                    
+Forwarding                    https://70e3-2600-1009-b1e6-a22f-a0b4-7cfc-6d81.ngrok.io -> http://localhost:3000         
+
+6. run eclipse from the command line to get env variables e.g. 
+open /Users/kroeders//eclipse/java-2021-12/Eclipse.app
+run the code, should see hello in the home screen
+
---
+++ src/main/java/us/categorize/Interlocutor.java
@@ -0,0 +1,53 @@
+package us.categorize;
+
+import static com.slack.api.model.block.Blocks.actions;
+import static com.slack.api.model.block.Blocks.asBlocks;
+import static com.slack.api.model.block.Blocks.divider;
+import static com.slack.api.model.block.Blocks.section;
+import static com.slack.api.model.block.composition.BlockCompositions.markdownText;
+import static com.slack.api.model.block.composition.BlockCompositions.plainText;
+import static com.slack.api.model.block.element.BlockElements.asElements;
+import static com.slack.api.model.block.element.BlockElements.button;
+import static com.slack.api.model.view.Views.view;
+
+import com.slack.api.bolt.App;
+import com.slack.api.bolt.jetty.SlackAppServer;
+import com.slack.api.model.event.AppHomeOpenedEvent;
+/**
+ * Hello world!
+ *
+ */
+public class Interlocutor 
+{
+    public static void main( String[] args ) throws Exception
+    {
+        var app = new App();
+        app.event(AppHomeOpenedEvent.class, (payload, ctx) -> {
+        	  var appHomeView = view(view -> view
+        	    .type("home")
+        	    .blocks(asBlocks(
+        	      section(section -> section.text(markdownText(mt -> mt.text("*This is running in eclipse :tada:")))),
+        	      divider(),
+        	      section(section -> section.text(markdownText(mt -> mt.text("This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example on <https://slack.dev/java-slack-sdk/guides/interactive-components|slack.dev/java-slack-sdk>.")))),
+        	      actions(actions -> actions
+        	        .elements(asElements(
+        	          button(b -> b.text(plainText(pt -> pt.text("Click me!"))).value("button1").actionId("button_1"))
+        	        ))
+        	      )
+        	    ))
+        	  );
+
+        	  var res = ctx.client().viewsPublish(r -> r
+        	    .userId(payload.getEvent().getUser())
+        	    .view(appHomeView)
+        	  );
+
+        	  return ctx.ack();
+        	});
+        for(String key : System.getenv().keySet()) {
+        	System.out.println(key + ","+System.getenv(key));
+        }
+        var server = new SlackAppServer(app);
+        server.start();
+    }
+}
